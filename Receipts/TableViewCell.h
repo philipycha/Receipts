a4ba7b520c51a2de9bd6049cd059935d5d0ba85d
philipycha <philip.yc.ha@gmail.com>
2016-09-29 12:03:07 -0700

    added constraints to tableviewcell

---
+++ Receipts/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  Receipts
+//
+//  Created by Philip Ha on 2016-09-29.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ Receipts/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  Receipts
+//
+//  Created by Philip Ha on 2016-09-29.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
